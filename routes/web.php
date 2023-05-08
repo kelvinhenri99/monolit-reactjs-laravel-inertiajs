0554f4d4b433190642f3601c6c2a33cf1f93dcaa
guilhermeabell <gpeuvis@gmail.com>
2023-05-07 22:37:28 -0300

    refactor:(master) - Refatorando rotas da aplicacao e permitindo acessar a api dentro da rota /api

--- routes/web.php
+++ routes/web.php
@@ -9,24 +9,31 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
+// Rota para acessar o client-side e server-side
 Route::get('/', HomeController::class)->name('home');
 
+// Rota para acessar apenas a API
+Route::prefix('api')->group(function () {
+    Route::middleware('auth')->group(function () {
+        Route::apiResource('users', UserController::class);
+    });
+
+    Route::middleware('guest')->group(function () {
+        Route::post('login', [LoginController::class, 'store']);
+        Route::post('register', [RegisterController::class, 'store']);
+        Route::get('auth/google', [GoogleController::class, 'redirectToGoogle'])->name('auth.google');
+        Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
+    });
+});
+
+// Rotas para autenticação
 Route::middleware('auth')->group(function () {
     Route::get('dashboard', DashboardController::class)->name('dashboard');
     Route::post('logout', [LoginController::class, 'destroy'])->name('logout');
-
-    Route::apiResource('users', UserController::class);
-
     Route::get('profile', ProfileController::class)->name('profile');
 });
 
 Route::middleware('guest')->group(function () {
     Route::get('login', [LoginController::class, 'create'])->name('login');
-    Route::post('login', [LoginController::class, 'store']);
-
     Route::get('register', [RegisterController::class, 'create'])->name('register');
-    Route::post('register', [RegisterController::class, 'store']);
-
-    Route::get('auth/google', [GoogleController::class, 'redirectToGoogle'])->name('auth.google');
-    Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
 });
